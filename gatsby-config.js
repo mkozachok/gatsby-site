a4c58a4022e106d30c20ff387676855829d50dce
mkozachok <epocalips009@gmail.com>
2019-10-11 14:15:43 +0300

    chore(): Configure seo, manifest and deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,13 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas eating things`,
+    description: `A simple description about pandas eating lots...`,
+    author: `Mykhaylo`,
   },
   plugins: [
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,6 +25,18 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        // icon: `src/images/icon.png`,
       },
     },
   ],
